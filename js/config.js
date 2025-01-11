e8624cacee037c06324f266eb10351b41f9b2e37
kusal374 <ms5377762@gmail.com>
2025-01-11 18:09:38 +0545

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     SHEETS: {
         PRODUCTS: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQpcR_bPO6q1IAoWvem5y5Fao2ilLyPY9A9Pt5UNpDsiBY2aefwxRjsSdsmV9uv54t8sVVNu9mjsJWo/pub?output=csv',
         GALLERY: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRnO1KPenxh_lI_9eMs27OO9sbXClHUmSbss36IrUbS6xxvwoaMpvdrc-R-TXHV3KtZAoKy22tVrFPG/pub?output=csv',
-        ABOUT: 'YOUR_GOOGLE_SHEET_ID_FOR_ABOUT'
+        ABOUT: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ17CUBA-CsDEbOlt-uzW68w8ZTCcHTdnkFQpULBNLSOWzsVmJNdzMJC2oX3cdV_KPHK1TzEN4PlQi5/pub?output=csv'
     },
     PHONE: '9864230589'
 };
