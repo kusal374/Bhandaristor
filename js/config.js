583cd177a557ff10000ad82ea264966bc7cb6e92
kusal374 <ms5377762@gmail.com>
2025-01-11 17:57:35 +0545

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     SHEETS: {
-        PRODUCTS: 'YOUR_GOOGLE_SHEET_ID_FOR_PRODUCTS',
-        GALLERY: 'YOUR_GOOGLE_SHEET_ID_FOR_GALLERY',
+        PRODUCTS: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQpcR_bPO6q1IAoWvem5y5Fao2ilLyPY9A9Pt5UNpDsiBY2aefwxRjsSdsmV9uv54t8sVVNu9mjsJWo/pub?output=csv',
+        GALLERY: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRnO1KPenxh_lI_9eMs27OO9sbXClHUmSbss36IrUbS6xxvwoaMpvdrc-R-TXHV3KtZAoKy22tVrFPG/pub?output=csv',
         ABOUT: 'YOUR_GOOGLE_SHEET_ID_FOR_ABOUT'
     },
     PHONE: '9864230589'
